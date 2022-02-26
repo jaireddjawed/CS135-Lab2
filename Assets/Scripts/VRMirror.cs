bde7733ad53cdd4643dc1f69f91d292ff867f8b7
Jaired Jawed <jairedjawed@icloud.com>
2022-02-25 20:06:25 -0800

    Face Rotates with camera

--- Assets/Scripts/VRMirror.cs
+++ Assets/Scripts/VRMirror.cs
@@ -4,32 +4,56 @@ using UnityEngine;
 
 public class VRMirror : MonoBehaviour
 {
-    public Camera camera;
-    Vector3 originalPos;
-    Quaternion originalRotation;
+    public OVRCameraRig camera;
+
+    Quaternion originalCameraRotation;
+
     bool mPressedOnce = false;
+    bool mPressedTwice = false;
+    bool rotateTime = false;
 
     // Start is called before the first frame update
     void Start()
     {
-      originalPos = Camera.main.transform.position;
-      originalRotation = Camera.main.transform.rotation;
+      originalCameraRotation = camera.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-      //Vector3 offset = camera.position - originalPos;
-      //this.position = this.position + offset;
-
       // pressing the m key once should make the cube match your movements
       if (Input.GetKeyDown(KeyCode.M)) {
         mPressedOnce = true;
+        rotateTime = true;
       }
 
-      if (mPressedOnce) {
-        //Vector3 cameraOffset = camera.position - originalPos;
-        //this.position = this.position + cameraOffset;
+      if (mPressedOnce && rotateTime) {
+        Debug.Log("Here");
+      transform.eulerAngles = new Vector3(
+        originalCameraRotation.x,
+        originalCameraRotation.y,
+        originalCameraRotation.z
+      );
+
+      rotateTime = false;
       }
+
+      /*if (mPressedOnce) {
+        float positionOffset = Vector3.Distance(Camera.main.transform.position, originalPos);
+        transform.position = transform.position + Camera.main.transform.forward * positionOffset * Time.deltaTime;
+
+        Quaternion newRotation = this.transform.rotation;
+        newRotation.y = Camera.main.transform.rotation.y;
+        this.transform.rotation = newRotation;
+
+        if (Input.GetKeyDown(KeyCode.M)) {
+          mPressedOnce = false;
+          mPressedTwice = true;
+        }
+      }
+
+      if (mPressedTwice) {
+
+      }*/
     }
 }
